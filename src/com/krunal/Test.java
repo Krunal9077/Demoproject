beac14c3f135a6435aeefabc2a7cc25e7d116fd8
krunal9077 <Krunalpatil581@gmail.com>
2023-01-18 19:13:24 +0530

    Added Test class

---
+++ src/com/krunal/Test.java
@@ -0,0 +1,15 @@
+package com.krunal;
+
+public class Test {
+int  a=10;
+int b = 10;
+public  void addition() {
+	System.out.println("addition of two no:"+(a+b));
+}
+
+	public static void main(String[] args) {
+	Test test = new Test();
+	test.addition();
+	}
+
+}
